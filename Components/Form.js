baaa5eac275b96ed21a5e6eb261391253a32c9c0
PxMach <moundjidmachghour58@gmail.com>
2025-07-28 15:15:29 +0100

    Ajout de la gestion des formulaires dans le composant Form : implémentation de la validation des champs, mise à jour des états pour fullname, email et github, et affichage des messages d'erreur correspondants.

--- Components/Form.js
+++ Components/Form.js
@@ -1,11 +1,56 @@
+"use client";
 import Image from "next/image";
 import React from "react";
 import upload from "../public/icon-upload.svg";
 import info from "../public/icon-info.svg";
 
 export default function Form() {
+  const [formData, setFormData] = React.useState({
+    fullname: "",
+    email: "",
+    github: "",
+  });
+
+  const [errors, setErrors] = React.useState({});
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validationErrors = {};
+    if (!formData.fullname.trim()) {
+      validationErrors.fullname = "Full name is required";
+    }
+
+    if (!formData.email.trim()) {
+      validationErrors.email = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.email)) {
+      validationErrors.email = "Please enter a valid Email adress";
+    }
+
+    if (!formData.github.trim()) {
+      validationErrors.github = "Github is required";
+    } else if (!/\S+@\S+\.\S+/.test(formData.github)) {
+      validationErrors.github = "Please enter a valid Github adress";
+    }
+
+    setErrors(validationErrors);
+    if (Object.keys(validationErrors).length === 0) {
+      alert("Form Submitted successfully");
+    }
+  };
+
   return (
-    <form action="get" className="mt-4 mb-8 flex flex-col gap-5 sm:w-sm">
+    <form
+      onSubmit={handleSubmit}
+      className="mt-4 mb-8 flex flex-col gap-5 sm:w-sm"
+    >
       <div className="flex flex-col gap-2">
         <label className="text-xl font-semibold text-gray-300">
           Upload Avatar
@@ -33,10 +78,14 @@ export default function Form() {
         <input
           className="bg-white/10 border-2 border-gray-500 rounded-xl p-3"
           type="text"
-          name="name"
+          name="fullname"
           placeholder="Full Name"
           aria-label="full name"
+          onChange={handleChange}
         />
+        {errors.fullname && (
+          <span className="text-red-500">{errors.fullname}</span>
+        )}
       </div>
 
       <div className="flex flex-col gap-2">
@@ -49,7 +98,9 @@ export default function Form() {
           name="email"
           placeholder=" example@email.com"
           aria-label="your email"
+          onChange={handleChange}
         />
+        {errors.email && <span className="text-red-500">{errors.email}</span>}
       </div>
 
       <div className="flex flex-col gap-2">
@@ -62,10 +113,15 @@ export default function Form() {
           name="github"
           placeholder="@yourusername"
           aria-label="github"
+          onChange={handleChange}
         />
+        {errors.github && <span className="text-red-500">{errors.github}</span>}
       </div>
 
-      <button className="bg-[#d96d49] border-gray-500 rounded-xl p-3 text-xl font-bold text-gray-950">
+      <button
+        type="submit"
+        className="bg-[#d96d49] border-gray-500 rounded-xl p-3 text-xl font-bold text-gray-950"
+      >
         Generate My Ticket
       </button>
     </form>
